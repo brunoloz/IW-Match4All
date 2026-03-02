dbd99f7a8e88fa445fd0ca29235ace8daed614f7
Parogo_72 <parogo004@gmail.com>
2026-03-02 18:58:01 +0100

    Add models

---
+++ src/main/java/es/ucm/fdi/iw/model/Equipo.java
@@ -0,0 +1,42 @@
+package es.ucm.fdi.iw.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Equipos")
+public class Equipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
+    @SequenceGenerator(name = "gen", sequenceName = "gen")
+    private long id;
+
+    @Column(name = "nom", nullable = false)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "id_capitan")
+    private User capitan;
+
+    @Column(length = 225)
+    private String escudo;
+
+    @OneToMany(mappedBy = "equipo")
+    private List<User> jugadores = new ArrayList<>();
+}
---
+++ src/main/java/es/ucm/fdi/iw/model/Partido.java
@@ -0,0 +1,48 @@
+package es.ucm.fdi.iw.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Partidos")
+public class Partido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
+    @SequenceGenerator(name = "gen", sequenceName = "gen")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_local", nullable = false)
+    private Equipo local;
+
+    @ManyToOne
+    @JoinColumn(name = "id_visitante", nullable = false)
+    private Equipo visitante;
+
+    @Column(name = "ubicacion", nullable = false, length = 225)
+    private String ubicacion;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    @Column(name = "id_competicion", nullable = false)
+    private Competicion idCompeticion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_arbitro", nullable = false)
+    private User arbitro;
+}
