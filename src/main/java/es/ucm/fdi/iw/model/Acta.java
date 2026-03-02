71af62b37caccacf49acb254fe4e1201380b200b
SiroFernandez <159046530+SiroFernandez@users.noreply.github.com>
2026-03-02 19:31:03 +0100

    Acta y ompeticion

---
+++ src/main/java/es/ucm/fdi/iw/model/Acta.java
@@ -0,0 +1,30 @@
+package es.ucm.fdi.iw.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Data
+public class Acta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "id_partido")
+    private long id_partido;
+    private long goles_local;
+    private long goles_visitante;
+
+    @OneToMany
+    @JoinColumn(name = "id_evento")
+    private long enventos;
+}
